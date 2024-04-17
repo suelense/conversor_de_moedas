9c2b68f0e5875db56e958eee2d7744b21c60c41c
suelense <suelense@gmail.com>
2024-04-17 19:22:58 -0300

    create new classes

--- src/br/com/conversor/models/GetRates.java
+++ src/br/com/conversor/models/GetRates.java
@@ -12,14 +12,9 @@ import java.net.http.HttpResponse;
 
 public class GetRates {
     // Setting URL
-    private String code = "";
     private String url = "https://v6.exchangerate-api.com/v6/2a922194323034021e38feba/latest/USD";
 
     public GetRates() throws IOException, InterruptedException {
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     // HttpClient
@@ -27,11 +22,11 @@ public class GetRates {
     HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-    JsonElement jelement = new JsonParser().parse(response.body());
-    JsonObject jobject = jelement.getAsJsonObject().getAsJsonObject("conversion_rates");
+    JsonElement jelement = JsonParser.parseString(response.body());
+    JsonObject rate = jelement.getAsJsonObject().getAsJsonObject("conversion_rates");
 
 
     public double getRate(String code) {
-        return(jobject.get(code).getAsDouble());
+        return(rate.get(code).getAsDouble());
     }
 }
